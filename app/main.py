b176e7161a413342b72c35e00f8cc324dac933ba
danceratopz <danceratopz@gmail.com>
2022-07-12 14:44:01 +0200

    feature: add supportedIATACodes and planeByCoordinates endpoints
    The plane emission API now also returns a consistent response model and returns 404 if a location can not be determined. Improved docstrings.

--- app/main.py
+++ app/main.py
@@ -5,9 +5,18 @@ from fastapi import FastAPI
 import app.plane_emissions as plane_emissions
 
 description = dedent(
-    """disCarbon's carbon emission API currently allows clients to request
-estimated emissions for plane travel."""
+    """disCarbon's carbon emission estimation API.
+       Currently, estimated emissions for plane travel are implemented.
+    """
 )
+
+tags_metadata = [
+    {"name": "Auxiliary", "description": "Auxiliary endpoints"},
+    {
+        "name": "Plane Emissions",
+        "description": "Plane travel carbon emission estimates",
+    },
+]
 
 emission_api = FastAPI(
     title="disCarbon Carbon Emission API",
@@ -21,10 +30,11 @@ emission_api = FastAPI(
         "name": "Apache 2.0",
         "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
     },
+    openapi_tags=tags_metadata,
 )
 
 
-@emission_api.get("/")
+@emission_api.get("/", tags=["Auxiliary"])
 async def root():
     """
     Return a helpful docstring pointing to the API's Swagger documentation if no
@@ -39,7 +49,7 @@ async def root():
 
 
 # If we have dependencies on other services, consider https://github.com/Kludex/fastapi-health
-@emission_api.get("/health")
+@emission_api.get("/health", tags=["Auxiliary"])
 async def app_health():
     """
     Basic health check to verify the API is still running.
@@ -47,19 +57,107 @@ async def app_health():
     return {"alive": True}
 
 
-@emission_api.get("/emissions/travel/planeByIATA/{from_airport}/{to_airport}/{travel_class}")
+@emission_api.get(
+    "/emissions/travel/supportedIATACodes",
+    tags=["Plane Emissions"],
+)
+async def supported_iata_codes():
+    """
+    Return a list of supported airport IATA codes.
+    """
+    response = {"iata_codes": plane_emissions.IATA_CODES_LIST}
+    return response
+
+
+@emission_api.get(
+    "/emissions/travel/planeByIATA/{from_airport}/{to_airport}/{travel_class}",
+    response_model=plane_emissions.PlaneEmissionResponse,
+    tags=["Plane Emissions"],
+)
 async def plane_emissions_by_iata(
     from_airport: plane_emissions.IATACodes,
     to_airport: plane_emissions.IATACodes,
     travel_class: plane_emissions.PlaneTravelClasses,
 ):
-    response = plane_emissions.calculate_emission(from_airport, to_airport, travel_class)
+    """
+    Estimate the carbon emissions for a single person flying between two airports
+    as specified by IATA codes.
+
+    The emission returned is for a one-way trip.
+
+    Example: <a href="https://api.discarbon.earth/emissions/travel/planeByIATA/LAX/BOG/economy">
+    LAX-BOG</a> - <br>
+    <a href="https://api.discarbon.earth/emissions/travel/planeByIATA/LAX/BOG/economy">
+    https://api.discarbon.earth/emissions/travel/planeByIATA/LAX/BOG/economy</a>
+
+    See also: <br>
+    <a href=
+    "https://api.discarbon.api/docs#/supported_iata_codes_emissions_travel_supportedIATACodes_get">
+    /emissions/travel/supportedIATACodes</a>
+    """
+    response = plane_emissions.calculate_emission_from_iata(from_airport, to_airport, travel_class)
     return response
 
 
-@emission_api.get("/emissions/travel/planeByCity/{from_city}/{to_city}/{travel_class}")
+@emission_api.get(
+    "/emissions/travel/planeByCoordinates/{from_lat}/{from_lon}/{to_lat}/{to_lon}/{travel_class}",
+    response_model=plane_emissions.PlaneEmissionResponse,
+    tags=["Plane Emissions"],
+)
+async def plane_emissions_by_lat_lon_coordinates(
+    from_lat: float,
+    from_lon: float,
+    to_lat: float,
+    to_lon: float,
+    travel_class: plane_emissions.PlaneTravelClasses,
+):
+    """
+    Estimate the carbon emissions for a single person flying between two locations
+    as specified by a decimal representation of its latitude and longitude coordinates.
+
+    As long as an address can be determined from the coordinates, they are taken literally,
+    i.e., it is not guaranteed that there is an airport located at the specified coordinates.
+    If no address can be determined, then the 404 status code is returned.
+
+    The emission returned is for a one-way trip.
+
+    Example, from (57.15,2.11) to (47.37,8.54) (LAX-BOG) - <br>
+    <a href=
+    "https://api.discarbon.earth/emissions/travel/planeByCoordinates/33.94/-118.42/4.70/-74.15/economy">
+    https://api.discarbon.earth/emissions/travel/planeByCoordinates/33.94/-118.42/4.70/-74.15/economy
+    </a>
+    """
+    from_coords = plane_emissions.Coord(from_lat, from_lon)
+    to_coords = plane_emissions.Coord(to_lat, to_lon)
+    response = plane_emissions.calculate_emission_from_coordinates(
+        from_coords, to_coords, travel_class
+    )
+    return response
+
+
+@emission_api.get(
+    "/emissions/travel/planeByCity/{from_city}/{to_city}/{travel_class}",
+    response_model=plane_emissions.PlaneEmissionResponse,
+    tags=["Plane Emissions"],
+)
 async def plane_emissions_by_city(
     from_city, to_city, travel_class: plane_emissions.PlaneTravelClasses
 ):
+    """
+    Estimate the carbon emissions for a single person flying between two airports
+    as specified by a location string.
+
+    The emission returned is for a one-way trip.
+
+    If no address from the provided string can be determined, then the 404 status code
+    is returned.
+
+    Example: From "Los Angeles International Airport" to "El Dorado International Airport"
+    (LAX-BOG) <br>
+    <a href=
+    "https://api.discarbon.earth/emissions/travel/planeByCity/Los%20Angeles%20International%20Airport/El%20Dorado%20International%20Airport/economy">
+    https://api.discarbon.earth/emissions/travel/planeByCity/Los%20Angeles%20International%20Airport/El%20Dorado%20International%20Airport/economy
+    </a>
+    """
     response = plane_emissions.calculate_emission_from_city(from_city, to_city, travel_class)
     return response
--- app/plane_emissions.py
+++ app/plane_emissions.py
@@ -5,9 +5,12 @@ Estimate carbon emissions for air travel.
 import math
 from collections import namedtuple
 from enum import Enum
+from typing import List, Union
 
 import pandas as pd
+from fastapi import HTTPException
 from geopy.geocoders import Nominatim
+from pydantic import BaseModel
 
 Coord = namedtuple("Coord", "lat lon")
 
@@ -23,9 +26,10 @@ def load_airport_data(filename):
 
 AIRPORTS = load_airport_data("resources/airports.csv")
 IATA_CODES_LIST = sorted([str(iata_code) for iata_code in AIRPORTS["iata_code"]])
-
-# TODO: This is slow and perhaps stupid? But it does provide excellent error checking and doc.
+# Note: This takes a few seconds.
 IATACodes = Enum("IATACodes", dict(zip(IATA_CODES_LIST, IATA_CODES_LIST)), type=str)
+
+geolocator = Nominatim(user_agent="MyApp")
 
 
 class PlaneTravelClasses(str, Enum):
@@ -38,12 +42,177 @@ class PlaneTravelClasses(str, Enum):
     first = "first"
 
 
-def calculate_emission(from_airport, to_airport, travel_class):
-    flight_distance = calculate_flight_distance(from_airport, to_airport)
-    emission = calculate_carbon_emission(flight_distance, travel_class)
+class PlaneEmissionResponse(BaseModel):
+    from_coords: List[float] = []
+    to_coords: List[float] = []
+    from_location: str
+    to_location: str
+    from_iata: Union[IATACodes, None] = None
+    to_iata: Union[IATACodes, None] = None
+    travel_class: PlaneTravelClasses
+    flight_distance: float
+    emission: float
+
+
+class PlaneTravel:
+    def __init__(self, from_coords: Coord, to_coords: Coord, travel_class: PlaneTravelClasses):
+        self.from_coords = from_coords
+        self.to_coords = to_coords
+        self.travel_class = travel_class
+
+    def calculate_geodesic_distance(self):
+        # Consider using geopy; https://stackoverflow.com/a/43211266
+        earth_radius = 6371.009  # km mean earth radius (spherical approximation)
+
+        from_coord = Coord(math.radians(self.from_coords.lat), math.radians(self.from_coords.lon))
+        to_coord = Coord(math.radians(self.to_coords.lat), math.radians(self.to_coords.lon))
+
+        delta_lambda = to_coord.lon - from_coord.lon
+
+        A = (math.cos(to_coord.lat) * math.sin(delta_lambda)) ** 2
+        B = (
+            math.cos(from_coord.lat) * math.sin(to_coord.lat)
+            - math.sin(from_coord.lat) * math.cos(to_coord.lat) * math.cos(delta_lambda)
+        ) ** 2
+        C = math.sin(from_coord.lat) * math.sin(to_coord.lat) + math.cos(from_coord.lat) * math.cos(
+            to_coord.lat
+        ) * math.cos(delta_lambda)
+
+        # Vyncenty's formula
+        delta_sigma = math.atan2(math.sqrt(A + B), C)
+        self.flight_distance = earth_radius * delta_sigma
+        return self.flight_distance
+
+    def calculate_carbon_emission(self):
+        """
+        Formula follows myClimate estimation calculator
+        emission parameters (short distance)
+        """
+        if self.flight_distance == 0:
+            # TODO error handling?
+            return
+        em_short = {
+            "a": 0,
+            "b": 2.714,
+            "c": 1166.52,
+            "S": 153.51,
+            "PLF": 0.82,
+            "DC": 95,
+            "CF": 0.07,
+            "CW": {"economy": 0.96, "business": 1.26, "first": 2.4},
+            "EF": 3.15,
+            "M": 2,
+            "P": 0.54,
+            "AF": 0.00038,
+            "A": 11.68,
+        }
+        # emission parameters (long distance)
+        em_long = {
+            "a": 0.0001,
+            "b": 7.104,
+            "c": 5044.93,
+            "S": 280.21,
+            "PLF": 0.82,
+            "DC": 95,
+            "CF": 0.23,
+            "CW": {"economy": 0.8, "business": 1.54, "first": 2.4},
+            "EF": 3.15,
+            "M": 2,
+            "P": 0.54,
+            "AF": 0.00038,
+            "A": 11.68,
+        }
+
+        emission = 0
+        if self.flight_distance < 1500:
+            # short distance
+            return self.single_emission_calc(em_short)
+        elif self.flight_distance > 2500:
+            # long distance
+            return self.single_emission_calc(em_long)
+        else:
+            # intermediate distance (interpolation)
+            short_emission = self.single_emission_calc(em_short)
+            long_emission = self.single_emission_calc(em_long)
+            long_dist_factor = (self.flight_distance - 1500) / 1000  # 0@1500km, 1@2500km
+            emission = (1 - long_dist_factor) * short_emission + long_emission * long_dist_factor
+        self.emission = emission
+        return self.emission
+
+    def single_emission_calc(self, em: dict):
+        emission = 0
+        d = self.flight_distance + em["DC"]
+        emission = (
+            ((em["a"] * d * d + em["b"] * d + em["c"]) / (em["S"] * em["PLF"]))
+            * (1 - em["CF"])
+            * em["CW"][self.travel_class]
+            * (em["EF"] * em["M"] + em["P"])
+            + em["AF"] * d
+            + em["A"]
+        )
+        emission /= 1000  # from kg to tonnes
+        return emission
+
+
+def get_coords_from_iata(airport_iata_code):
+    """
+    Get the latitudal and longtidual coordinates for the airport specified by
+    provided IATA code.
+    """
+    coords = AIRPORTS.query(f"iata_code == '{airport_iata_code}'")[
+        ["latitude_deg", "longitude_deg"]
+    ]
+    return Coord(coords.iloc[0]["latitude_deg"], coords.iloc[0]["longitude_deg"])
+
+
+def calculate_emission_from_iata(
+    from_airport: IATACodes, to_airport: IATACodes, travel_class: PlaneTravelClasses
+):
+    from_coords = get_coords_from_iata(from_airport)
+    to_coords = get_coords_from_iata(to_airport)
+    plane_travel = PlaneTravel(from_coords, to_coords, travel_class)
+    flight_distance = plane_travel.calculate_geodesic_distance()
+    emission = plane_travel.calculate_carbon_emission()
+    # complete the response
+    from_location = geolocator.reverse(f"{from_coords.lat}, {from_coords.lon}").address
+    to_location = geolocator.reverse(f"{to_coords.lat}, {to_coords.lon}").address
     response = {
-        "from_airport": from_airport,
-        "to_airport": to_airport,
+        "from_iata": from_airport,
+        "to_iata": to_airport,
+        "from_coords": from_coords,
+        "to_coords": to_coords,
+        "from_location": from_location,
+        "to_location": to_location,
+        "travel_class": travel_class,
+        "flight_distance": flight_distance,
+        "emission": emission,
+    }
+    return response
+
+
+def calculate_emission_from_coordinates(from_coords, to_coords, travel_class):
+    plane_travel = PlaneTravel(from_coords, to_coords, travel_class)
+    flight_distance = plane_travel.calculate_geodesic_distance()
+    emission = plane_travel.calculate_carbon_emission()
+    # complete the response
+    from_location = geolocator.reverse(f"{from_coords.lat}, {from_coords.lon}")
+    if not from_location:
+        raise HTTPException(
+            status_code=404,
+            detail=f"no address could be determined from the from_coords '{from_coords}'",
+        )
+    to_location = geolocator.reverse(f"{to_coords.lat}, {to_coords.lon}")
+    if not to_location:
+        raise HTTPException(
+            status_code=404,
+            detail=f"no address could be determined from the to_coords '{to_coords}'",
+        )
+    response = {
+        "from_coords": from_coords,
+        "to_coords": to_coords,
+        "from_location": from_location.address,
+        "to_location": to_location.address,
+        "travel_class": travel_class,
         "flight_distance": flight_distance,
         "emission": emission,
     }
@@ -51,134 +220,30 @@ def calculate_emission(from_airport, to_airport, travel_class):
 
 
 def calculate_emission_from_city(from_city, to_city, travel_class):
-    geolocator = Nominatim(user_agent="MyApp")
     from_location = geolocator.geocode(from_city)
+    if not from_location:
+        raise HTTPException(status_code=404, detail=f"from_city '{from_city}' not found")
     to_location = geolocator.geocode(to_city)
-    from_coord = Coord(from_location.latitude, from_location.longitude)
-    to_coord = Coord(to_location.latitude, to_location.longitude)
-    flight_distance = calculate_geodesic_distance(from_coord, to_coord)
-    emission = calculate_carbon_emission(flight_distance, travel_class)
+    if not to_location:
+        raise HTTPException(status_code=404, detail=f"to_city '{to_city}' not found")
+    from_coords = Coord(from_location.latitude, from_location.longitude)
+    to_coords = Coord(to_location.latitude, to_location.longitude)
+    plane_travel = PlaneTravel(from_coords, to_coords, travel_class)
+    flight_distance = plane_travel.calculate_geodesic_distance()
+    emission = plane_travel.calculate_carbon_emission()
     response = {
-        "from_location": repr(from_location.address),
-        "to_location": repr(to_location.address),
+        "from_coords": from_coords,
+        "to_coords": to_coords,
+        "from_location": from_location.address,
+        "to_location": to_location.address,
+        "travel_class": travel_class,
         "flight_distance": flight_distance,
         "emission": emission,
     }
     return response
 
 
-def calculate_flight_distance(from_airport, to_airport):
-    """
-    Get flight distance from the two airport input fields
-    """
-    from_location = AIRPORTS.query(f"iata_code == '{from_airport}'")[
-        ["latitude_deg", "longitude_deg"]
-    ]
-    to_location = AIRPORTS.query(f"iata_code == '{to_airport}'")[["latitude_deg", "longitude_deg"]]
-
-    from_coord = Coord(
-        from_location.iloc[0]["latitude_deg"], from_location.iloc[0]["longitude_deg"]
-    )
-    to_coord = Coord(to_location.iloc[0]["latitude_deg"], to_location.iloc[0]["longitude_deg"])
-
-    return calculate_geodesic_distance(from_coord, to_coord)
-
-
-def calculate_geodesic_distance(from_location, to_location):
-    # Consider using geopy; https://stackoverflow.com/a/43211266
-    earth_radius = 6371.009  # km mean earth radius (spherical approximation)
-
-    from_coord = Coord(math.radians(from_location.lat), math.radians(from_location.lon))
-    to_coord = Coord(math.radians(to_location.lat), math.radians(to_location.lon))
-
-    delta_lambda = to_coord.lon - from_coord.lon
-
-    A = (math.cos(to_coord.lat) * math.sin(delta_lambda)) ** 2
-    B = (
-        math.cos(from_coord.lat) * math.sin(to_coord.lat)
-        - math.sin(from_coord.lat) * math.cos(to_coord.lat) * math.cos(delta_lambda)
-    ) ** 2
-    C = math.sin(from_coord.lat) * math.sin(to_coord.lat) + math.cos(from_coord.lat) * math.cos(
-        to_coord.lat
-    ) * math.cos(delta_lambda)
-
-    # Vyncenty formula
-    delta_sigma = math.atan2(math.sqrt(A + B), C)
-    distance = earth_radius * delta_sigma
-    return distance
-
-
-def calculate_carbon_emission(flight_distance: float, travel_class: PlaneTravelClasses):
-    """
-    Formula follows myclimate estimation calculator
-    emission parameters (short distance)
-    """
-    if flight_distance == 0:
-        # TODO error handling?
-        return
-    em_short = {
-        "a": 0,
-        "b": 2.714,
-        "c": 1166.52,
-        "S": 153.51,
-        "PLF": 0.82,
-        "DC": 95,
-        "CF": 0.07,
-        "CW": {"economy": 0.96, "business": 1.26, "first": 2.4},
-        "EF": 3.15,
-        "M": 2,
-        "P": 0.54,
-        "AF": 0.00038,
-        "A": 11.68,
-    }
-    # emission parameters (long distance)
-    em_long = {
-        "a": 0.0001,
-        "b": 7.104,
-        "c": 5044.93,
-        "S": 280.21,
-        "PLF": 0.82,
-        "DC": 95,
-        "CF": 0.23,
-        "CW": {"economy": 0.8, "business": 1.54, "first": 2.4},
-        "EF": 3.15,
-        "M": 2,
-        "P": 0.54,
-        "AF": 0.00038,
-        "A": 11.68,
-    }
-
-    emission = 0
-    if flight_distance < 1500:
-        # short distance
-        return single_emission_calc(em_short, flight_distance, travel_class)
-    elif flight_distance > 2500:
-        # long distance
-        return single_emission_calc(em_long, flight_distance, travel_class)
-    else:
-        # intermediate distance (interpolation)
-        short_emission = single_emission_calc(em_short, flight_distance, travel_class)
-        long_emission = single_emission_calc(em_long, flight_distance, travel_class)
-        long_dist_factor = (flight_distance - 1500) / 1000  # 0@1500km, 1@2500km
-        emission = (1 - long_dist_factor) * short_emission + long_emission * long_dist_factor
-    return emission
-
-
-def single_emission_calc(em: dict, flight_distance: float, travel_class: PlaneTravelClasses):
-    emission = 0
-    d = flight_distance + em["DC"]
-    emission = (
-        ((em["a"] * d * d + em["b"] * d + em["c"]) / (em["S"] * em["PLF"]))
-        * (1 - em["CF"])
-        * em["CW"][travel_class]
-        * (em["EF"] * em["M"] + em["P"])
-        + em["AF"] * d
-        + em["A"]
-    )
-    emission /= 1000  # from kg to tonnes
-    return emission
-
-
 if __name__ == "__main__":
-    resp = calculate_emission("ZRH", "LAX", PlaneTravelClasses.economy)
+
+    resp = calculate_emission_from_iata("ZRH", "LAX", PlaneTravelClasses.economy)
     print(resp)
